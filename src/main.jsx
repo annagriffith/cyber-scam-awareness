693e835abee248d1f17fa40a0826bb06b19e3e14
Sana <manshadisana@gmail.com>
2026-09-03 15:25:12 +1000

    modified:   src/main.jsx 	modified:   src/pages/Home.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Home from './pages/Home.jsx'
 import HowToPlay from './pages/HowToPlay.jsx'
+import Gameplay from './Gameplay.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/how-to-play" element={<HowToPlay />} />
+        <Route path="/gameplay" element={<Gameplay />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
